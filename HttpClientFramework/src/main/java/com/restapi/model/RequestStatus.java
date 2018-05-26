ba6d5fd691d2296ff871509e12c66fad7f8d8d65
rajesh <raju.cva@gmail.com>
2018-05-26 11:21:41 +0530

    added override functions for futurecallback class

--- HttpClientFramework/src/main/java/com/restapi/model/RequestStatus.java
+++ HttpClientFramework/src/main/java/com/restapi/model/RequestStatus.java
@@ -1,5 +1,26 @@
 package com.restapi.model;
 
-public class RequestStatus {
+import org.apache.http.HttpResponse;
+import org.apache.http.concurrent.FutureCallback;
+
+public class RequestStatus implements FutureCallback<HttpResponse>{
+
+	@Override
+	public void completed(HttpResponse result) {
+		// TODO Auto-generated method stub
+		System.out.println("===============Reqeust Completed======="+result.getStatusLine().getStatusCode());
+	}
+
+	@Override
+	public void failed(Exception ex) {
+		// TODO Auto-generated method stub
+		System.out.println("===============Reqeust Failed======="+ex.getMessage());
+	}
+
+	@Override
+	public void cancelled() {
+		// TODO Auto-generated method stub
+		System.out.println("===============Reqeust Cancelled=======");
+	}
 
 }
